f1e8cef02b1227f8d07b752a1793a5ff270f9e08
racs11 <raulantoniocastillejos@hotmail.com>
2023-05-26 14:40:02 -0600

    added sidebar and multiple pages

--- RegularGrammar_to_FSA/app.R
+++ RegularGrammar_to_FSA/app.R
@@ -8,17 +8,47 @@
 #
 
 library(shiny)
-if (interactive()) {
-  
-  ui <- fluidPage(
-    textInput("Texto", "Automata", ""),
-    verbatimTextOutput("value")
+library(igraph)
+library(shinydashboard)
+
+
+
+
+
+ui <- dashboardPage(
+    dashboardHeader(title = "Proyecto 3 "),
+    dashboardSidebar(
+      menuItem("Convertidor de grámatica", tabName = "convertidor", icon = icon("th")),
+      menuItem("Verificador de automata finito determinista", tabName = "verificador", icon = icon("th"))
+    ),
+    dashboardBody(
+      tabItems(
+        # First tab content
+        tabItem(tabName = "convertidor",
+                box(
+                  textInput("Texto", "Automata a plotear", ""),
+                  verbatimTextOutput("value")
+                )
+                ),
+        
+        
+        # Second tab content
+        tabItem(tabName = "verificador",
+                box(
+                  textInput("Texto", "Automata a verificar", ""),
+                  verbatimTextOutput("value")
+                )
+                )
+      )
+      )
   )
-  server <- function(input, output) {
-    output$value <- renderText({ input$Texto })
-  }
-  shinyApp(ui, server)
+    
+server <- function(input, output) {
+  output$value <- renderText({ input$Texto })
+  
 }
+shinyApp(ui, server)
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
