58ff62d06543d6e767ce7aab8de911eefe277be8
Davide Dunne <111002142+DavideDunne@users.noreply.github.com>
2023-06-01 23:00:14 -0600

    Add build node array matrix func

--- RegularGrammar_to_FSA/functions.R
+++ RegularGrammar_to_FSA/functions.R
@@ -1,3 +1,27 @@
-build.nodes -> function(parts) {
+build.nodes -> function(tran) {
+  nodes <- c()
+  edges <- c()
   
+  for(i in 1:length(tran)){
+    #Get rid of arrow
+    tran[i] <- gsub("->", "", tran[i])
+    #Get rid of spaces
+    tran[i] <- gsub(" ","", tran[i])
+    
+    edges <- c(edges, substring(tran[i],2,2))
+    if(nchar(tran[i]) == 3){
+      nodes <- c(nodes, substring(tran[i],1,1), substring(tran[i],3,3))
+    }
+    else{
+      nodes <- c(nodes, substring(tran[i],1,1), "Z")
+    }
+  }
+  unique.nodes <- unique(nodes)
+  tam <- length(unique.nodes)
+  type.node <- rep(1, tam)
+  type.node["S"==unique.nodes]<- 2
+  type.node["Z"==unique.nodes]<- 3
+  
+  list.column <- list(nodes = nodes, edges = edges, type.node = type.node)
+  return(list.column)
 }
